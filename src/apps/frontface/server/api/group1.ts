87960f4d52abf7feb5b9cc2308e529bccb7c3685
Beam-1 <66309010022@loeitech.ac.th>
2024-04-18 23:42:02 +0700

    opjk

---
+++ src/apps/frontface/server/api/group1.ts
@@ -0,0 +1,22 @@
+export default defineEventHandler(async (event) => {
+    return {
+     mename: "กัญญารัตน์ สาระพันธ์",
+     housenb: 506,
+     group: 5,
+     road: "มิตรภาพ",
+     subdistrict: "ศิลา",
+     district: "เมือง",
+     province: "ขอนแก่น",
+     zipcode: 40000,
+     phonenb: '096555542365',
+     age: 20,
+     birthday: '02/05/2004',
+     idcard: '1429900507767',
+     idaccount: '06547855551',
+     position: "อาจารย์",
+     department: "มหาลัย",
+     salary: 20050,
+
+      dateReceived: new Date().toISOString(),
+    }
+  });
