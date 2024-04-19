924727463bd07283421aa8577e2e4bb0e7a528a1
Jane1m <rinda@finsoft2023.com>
2024-04-19 09:24:17 +0700

    add emergencyLoan page

---
+++ src/apps/frontface/pages/paper/emergencyLoan.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+      <PaperDoc>
+        <h3>กู้ฉุกเฉินหน้าที่ 1</h3>
+       
+      </PaperDoc>
+  
+      
+      <MorePaperDoc>  
+        <h3>กู้ฉุกเฉินหน้าที่ 1</h3>
+     
+       
+      </MorePaperDoc>
+    </main>
+  </template>
+  
+  <script setup lang="ts">
+  import Index from '../index.vue';
+  
+  const { data } = await useFetch<any>('/api/sampleWithData');
+  </script>
